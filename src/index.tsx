4bee6f8534ab3cb21ca1fdec16231ac6a2cc9871
maxim_bredikhin <maxzb86@gmail.com>
2022-02-05 00:14:42 +0300

    Chapter "Modifying data"

---
+++ src/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { render } from "react-dom";
+import { getSnapshot, types } from "mobx-state-tree";
+// import { observer } from "mobx-react";
+
+const Todo = types
+  .model({
+    name: types.optional(types.string, ""),
+    done: types.optional(types.boolean, false),
+  })
+  .actions((self) => ({
+    setName(newName: string) {
+      self.name = newName;
+    },
+
+    toggle() {
+      self.done = !self.done;
+    },
+  }));
+
+const User = types.model({
+  name: types.optional(types.string, ""),
+});
+
+const john = User.create();
+const eat = Todo.create({ name: "new eat" });
+
+const RootStore = {
+  users: types.map(User),
+  todos: types.optional(types.map(Todo), {}),
+};
+
+const store = {
+  users: {},
+};
+
+console.log("John:", getSnapshot(john));
+console.log("Eat TODO:", getSnapshot(eat));
+
+render(
+  <div>
+    John: {JSON.stringify(getSnapshot(john))}
+    <br />
+    Eat TODO: {JSON.stringify(getSnapshot(eat))}
+  </div>,
+  document.getElementById("root")
+);
